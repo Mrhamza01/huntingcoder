f10b7f1343605e9e7df5c0bb4f668eb4d287bdf9
hamza <hamza.kamboh035@gmail.com>
2022-11-10 15:23:45 +0500

    nav bar added with boootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
+import PNavbar from '../components/PNavbar.js'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <>  
+  <PNavbar/>
+  <Component {...pageProps} />
+  </>
 }
